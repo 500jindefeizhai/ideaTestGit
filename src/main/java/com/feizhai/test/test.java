b9f60193d4a2a5bb4589025d7f51ea4069631f60
feizhai <1038900539@qq.com>
2021-02-06 16:01:46 +0800

    hot_fix

--- src/main/java/com/feizhai/test/test.java
+++ src/main/java/com/feizhai/test/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello world! 开发人员");
         System.out.println("修改");
+        System.out.println("fix_hot 修改");
     }
 }
