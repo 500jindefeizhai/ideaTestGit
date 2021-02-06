a78d7ea7f2e277d729ff80024f0ad980f270282d
feizhai <1038900539@qq.com>
2021-02-06 15:01:27 +0800

    项目经理修改

--- src/main/java/com/feizhai/test/test.java
+++ src/main/java/com/feizhai/test/test.java
@@ -2,7 +2,7 @@ package com.feizhai.test;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("hello world! 项目经理");
         System.out.println("修改");
     }
 }
