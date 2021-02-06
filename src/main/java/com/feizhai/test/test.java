6cf8f75b387372f1abab10a1b8c575fc4aad3a41
feizhai <1038900539@qq.com>
2021-02-06 15:02:15 +0800

    开发人员提交

--- src/main/java/com/feizhai/test/test.java
+++ src/main/java/com/feizhai/test/test.java
@@ -2,7 +2,7 @@ package com.feizhai.test;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("hello world! 开发人员");
         System.out.println("修改");
     }
 }
